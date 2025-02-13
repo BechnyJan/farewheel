5810c8bdcc0c991ce8893974a1e96eac919c51bf
BechnyJan <janb.echny@seznam.cz>
2025-02-13 14:31:19 +0100

    navigation update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Tourist from "../icons/tourist.svg";
-import classes from './Header.module.css'
+import classes from "./Header.module.css";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const accountHandler = () => {
+    navigate("/signin");
+  };
+
+  const homeHandler = () => {
+    navigate('/')
+  }
   return (
     <header>
-      <h1>FareWheel</h1>
-      <img src={Tourist} alt="account" />
+      <h1 onClick={homeHandler}>FareWheel</h1>
+      <label onClick={accountHandler} htmlFor="auth">
+        <img src={Tourist} id="auth" alt="account" />
+      </label>
     </header>
   );
 }
